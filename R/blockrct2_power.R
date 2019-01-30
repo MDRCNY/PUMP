e6fc70db7bc3062f61544cfca115038712247302
Zarni Htet <Zarni.Htet@mdrc.org>
2019-01-30 16:53:57 -0500

    Documented 2 helper functions and the workhorse power function

--- R/blockrct2_power.R
+++ R/blockrct2_power.R
@@ -14,9 +14,14 @@ if(!exists("grab.pval", mode = "function")) source("R/utils.R")
 #'
 comp.rawt.SS <- function(abs.Zs.H0.1row, abs.Zs.H1.1samp, oo) {
 
-  M<-length(abs.Zs.H0.1row)
+  M <- length(abs.Zs.H0.1row)
   maxt <- rep(NA, M)
-  for (m in 1:M) {maxt[m] <- max(abs.Zs.H0.1row[m]) > abs.Zs.H1.1samp[m]}
+  browser()
+  for (m in 1:M) {
+      browser()
+      maxt[m] <- max(abs.Zs.H0.1row[m]) > abs.Zs.H1.1samp[m]
+
+  }
   return(as.integer(maxt))
 }
 
@@ -41,6 +46,8 @@ comp.rawt.SD <- function(abs.Zs.H0.1row, abs.Zs.H1.1samp, oo) {
 
 #' Adjust Single Step WestFallYoung
 #'
+#' This function implements
+#'
 #' @param snum blah blah
 #' @param abs.Zs.H0 blah blah
 #' @param abs.Zs.H1 blah blah
@@ -51,9 +58,14 @@ comp.rawt.SD <- function(abs.Zs.H0.1row, abs.Zs.H1.1samp, oo) {
 adjust.allsamps.WYSS<-function(snum,abs.Zs.H0,abs.Zs.H1) {
 
   adjp.WY<-matrix(NA,snum,ncol(abs.Zs.H0))
+  browser()
   doWY<-for (s in 1:snum) {
+
+    browser()
     ind.B<-t(apply(abs.Zs.H0, 1, comp.rawt.SS, abs.Zs.H1.1samp=abs.Zs.H1[s,]))
+    browser()
     adjp.WY[s,]<-colMeans(ind.B)
+
   }
   return(adjp.WY)
 }
@@ -85,7 +97,7 @@ adjust.allsamps.WYSD<-function(snum,abs.Zs.H0,abs.Zs.H1,order.matrix,ncl) {
   doWY <- foreach::foreach(s= 1:snum, .combine=rbind) %dopar% {
     ind.B<-t(apply(abs.Zs.H0, 1, comp.rawt.SD, abs.Zs.H1.1samp=abs.Zs.H1[s,], oo=order.matrix[s,]))
     pi.p.m <- colMeans(ind.B)
-    browser()
+    #browser()
     # enforcing monotonicity
     adjp.minp <- numeric(M)
     adjp.minp[1] <- pi.p.m[1]
@@ -97,15 +109,17 @@ adjust.allsamps.WYSD<-function(snum,abs.Zs.H0,abs.Zs.H1,order.matrix,ncl) {
 }
 
 
-#' t.mean.h1 helper function
+#' t.mean.h1 function for generating the mean of test statistics under the joint alternative hypothesis
 #'
-#' @param MDES blah blah
-#' @param J blah blah
-#' @param n.j blah blah
-#' @param R2.1 blah blah
-#' @param p blah blah
+#' The function t.mean.H1 computes the means of the test statistics under the joint alternative hypothesis.
 #'
-#' @return blah blah
+#' @param MDES  a vector of length M corresponding to the minimum detectable effect sizes (MDESs) for the M outcomes
+#' @param J the number of blocks
+#' @param n.j the harmonic means of the number of units per block
+#' @param R2.1 a vector of length M corresponding to R^2 for Level-1 covariates for M outcomes
+#' @param p the proportion of test statistics assigned to treatment within each block group
+#'
+#' @return mean of the test statistics under the joint alternative hypothesis
 
 t.mean.H1<-function(MDES,J,n.j,R2.1,p) {
 
@@ -114,11 +128,13 @@ t.mean.H1<-function(MDES,J,n.j,R2.1,p) {
 
 #' Degrees of Freedom
 #'
-#' @param J blah blah
-#' @param n.j blah blah
-#' @param numCovar.1 blah
+#' This function calculates the degree of freedom for 2 level RCT block design
 #'
-#' @return blah blah
+#' @param J number of blocks
+#' @param n.j units per block
+#' @param numCovar.1 number of Level 1 baseline covariates (not including block dummies)
+#'
+#' @return the degree of freedom
 
 df<-function(J,n.j,numCovar.1) {
 
@@ -128,71 +144,75 @@ df<-function(J,n.j,numCovar.1) {
 
 #' Block RCT2 power function
 #'
-#' @param M the number of hypothesis tests (outcomes)
-#' @param MDES a single entry vector detailing the minimum detectable effect size
-#' @param Ai a single entry vector detailing the number of outcomes with a non-zero effect
-#' @param J number of blocks
-#' @param n.j units per block
-#' @param p the proportion of samples that are assigned the treatment
-#' @param alpha significance level
-#' @param numCovar.1 number of Level 1 baseline covariates (not including block dummies)
-#' @param numCovar.2 number of Level 2 baseline covariates
-#' @param R2.1 a vector of length M corresponding to R^2 for M outcomes of Level 1(R^2 = variation in the data explained by the model)
-#' @param R2.2 a vector of length M corresponding to R^2 for M outcomes of Level 2
-#' @param ICC intraclass correlation
-#' @param mod.type "c" for constant effects, "f" for fixed effects, "r" for random effects
-#' @param sigma correlation matrix for correlations between test statistics (this need to be flexible across multiple test statistics. Now, it's set to be at 1)
-#' @param omega NULL
-#' @param tnum number of test statistics (samples) for all procedures other than WY & number of permutations for WY (i.e permutation samples) WY has permutation which is reassignment of treatment variables. We do not see the treatment reassignment for permutation explicitly here.
-#' @param snum number of samples for WY (i.e are they less than the other test. If so, why?)
-#' @param ncl the number of clusters to use for parallel processing. It has a default of 2.
-#' @param updateProgress the callback function to update the progress bar
-#' @importFrom multtest mt.rawp2adjp
-#' @return power results
-#' @export
+#' This functions calculates power for all definitions of power (individual, d-minimal, complete) for all the different MTPs
+#' (Bonferroni, Holms, Bejamini-Hocheberg, Westfall-Young Single Step, Westfall-Young Step Down). The function works for
+#' 2-Level Block RCT
 #'
+#' @param M the number of hypothesis tests (outcomes)
+#' @param MDES a vector of length M corresponding to the MDESs for the M outcomes
+#' @param Ai a single entry vector specifying the estimated number of outcomes with a non-zero effect
+#' @param J the number of blocks
+#' @param n.j the harmonic mean of the number of units per block
+#' @param p the proportion of samples that are assigned to the treatment
+#' @param alpha the family wise error rate (FWER)
+#' @param numCovar.1 number of Level 1 baseline covariates (not including block dummies)
+#' @param numCovar.2 number of Level 2 baseline covariates (set to 0 for this design)
+#' @param R2.1 a vector of length M corresponding to R^2 for M outcomes of Level 1 (R^2 = variation in the data explained by the model)
+#' @param R2.2 a vector of length M corresponding to R^2 for M outcomes of Level 2 (R^2 = variation in the data explained by the model)
+#' @param ICC intraclass correlation
+#' @param mod.type "c" for constant effects, "f" for fixed effects, "r" for random effects (parameter not in use at the moment)
+#' @param sigma correlation matrix for correlations between test statistics (parameter not in use at the moment.Default is set to 0.99)
+#' @param omega NULL (parameter in development)
+#' @param tnum the number of test statistics (samples) for all procedures other than Westfall-Young & number of permutations for WY. The default is set at 10,000
+#' @param snum the number of samples for Westfall-Young. The default is set at 1,000.
+#' @param ncl the number of clusters to use for parallel processing. The default is set at 2.
+#' @param updateProgress the callback function to update the progress bar (User does not have to input anything)
+#' @importFrom multtest mt.rawp2adjp
+#' @return power results across all definitions of power and MTP
+#' @export
 
 power.blockedRCT.2<-function(M, MDES, Ai, J, n.j,
                              p, alpha, numCovar.1, numCovar.2=0, R2.1, R2.2 = NULL, ICC,
                              mod.type, sigma = 0, omega = NULL,
                              tnum = 10000, snum=1000, ncl=2, updateProgress = NULL) {
 
-  #Output error statement in R
+  # Error handling when user put in actual effect number that is greater than the total number of outcomes
   if( Ai > M){
 
-    stop('The number of outcomes with actual effects cannot be greater than the total number of outcomes of an experiment. Please adjust your inputs.')
+    stop('The number of outcomes with actual effects cannot be greater than the total number of outcomes of an experiment.
+         Please adjust your inputs.')
 
-  } # Error handling for when actual effect number is greater than the total number of outcomes
-
-  #browser()
+  } # end of if statement
 
   # MDES must be the length of Actual Impacts
   MDES <- rep(MDES,Ai)
 
-  # the difference between the length of M and Ai should be zero
+  # the difference between the length of M and Ai would be zero as they do not have any impacts
   noeffect <- rep(0, M-Ai)
   MDES <- c(MDES, noeffect)
 
-  # Setting Sigma up
+  # Setting a default Sigma up
   sigma <- matrix(0.99, M, M)
   diag(sigma) <- 1
 
-  #browser()
+  # number of false nulls (i.e they are really not nulls)
+  numfalse <- sum(1*MDES>0)
 
-  # number of false nulls
-  numfalse<-sum(1*MDES>0)
-
-  # compute Q(m) for all false nulls
+  # compute Q(m) for all false nulls. We are calculating the test statistics for when the alternative hypothesis is true.
   t.shift<-t.mean.H1(MDES,J,n.j,R2.1,p)
   t.df<-df(J,n.j,numCovar.1)
   t.shift.mat<-t(matrix(rep(t.shift,tnum),M,tnum)) # repeating shift.beta on every row
 
-
   # generate test statistics and p-values under null and alternative $s=\frac{1}{2}$
+  # rmvt draws from a multivariate t-distribution
   Zs.H0<- mvtnorm::rmvt(tnum, sigma = sigma, df = t.df, delta = rep(0,M),type = c("shifted", "Kshirsagar"))
   Zs.H1 <- Zs.H0 + t.shift.mat
+
+  # calculates p values from quantiles and degrees of freedom
   pvals.H0<- stats::pt(-abs(Zs.H0),df=t.df) * 2
   pvals.H1<- stats::pt(-abs(Zs.H1),df=t.df) * 2
+
+  # getting the absolute values of the test statistics
   abs.Zs.H0 <- abs(Zs.H0)
   abs.Zs.H1 <- abs(Zs.H1)
 
@@ -202,23 +222,29 @@ power.blockedRCT.2<-function(M, MDES, Ai, J, n.j,
     updateProgress(message = msg) # Passing back the progress messages onto the callback function
   } # if the function is being called, run the progress bar
 
-  # adjust p-values for all but Westfall-Young
+
+  # explicit invocation of the function and saving it so that we can pass it along in apply without the
+  # explicit calling inside apply
   mt.rawp2adjp <- multtest::mt.rawp2adjp
+
   #adjp<-multtest::mt.rawp2adjp(pvals.H1,proc=c("Bonferroni","Holm","BH"),alpha=alpha)
   #rawp <- grab.pval(adjp, proc = "rawp")
   #adjp.BF <- grab.pval(adjp, proc = "Bonferroni")
   #adjp.HO <- grab.pval(adjp, proc = "Holm")
   #adjp.BH <- grab.pval(adjp, proc = "BH")
 
-  # adjust p-values for all but Westfall-Young
+  # adjust p-values for all but Westfall-Young using multtest's adjustment function
+  # via Bonferroni, Holm and Bejamini-Hocheberg
   adjp<-apply(pvals.H1,1,mt.rawp2adjp,proc=c("Bonferroni","Holm","BH"),alpha=alpha)
+
+  # seperating out p values that are adjusted by Bonferroni, Holm and Benjamini-Hocheberg
   grab.pval<-function(...,proc) {return(...$adjp[order(...$index),proc])}
   rawp<-do.call(rbind,lapply(adjp,grab.pval,proc="rawp"))
   adjp.BF<-do.call(rbind,lapply(adjp,grab.pval,proc="Bonferroni"))
   adjp.HO<-do.call(rbind,lapply(adjp,grab.pval,proc="Holm"))
   adjp.BH<-do.call(rbind,lapply(adjp,grab.pval,proc="BH"))
 
-  #2nd call back to progress bar on adjustments except WestFall Young is done
+  # 2nd call back to progress bar on adjustments except WestFall Young is done
   if (is.function(updateProgress) & !is.null(adjp.BH)) {
     msg  <- paste0("Multiple adjustments done except for WestFall Young.") # Priamry text we want to display
     updateProgress(message = msg) # Passing back the progress messages onto the callback function
@@ -228,35 +254,38 @@ power.blockedRCT.2<-function(M, MDES, Ai, J, n.j,
   order.matrix<-t(apply(abs.Zs.H1,1,order,decreasing=TRUE))
   adjp.SS<-adjust.allsamps.WYSS(snum,abs.Zs.H0,abs.Zs.H1)
   adjp.WY<-adjust.allsamps.WYSD(snum,abs.Zs.H0,abs.Zs.H1,order.matrix,ncl)
-  # combine all adjusted p-values in list (each entry is matrix for given MTP)
+
+  # combine all adjusted p-values in list (each entry is a matrix for given MTP)
   adjp.all<-list(rawp,adjp.BF,adjp.HO,adjp.BH,adjp.SS,adjp.WY)
 
-  #3rd call back to progress bar on adjustments: Westfall Young Single Step and Step Down is done
+  # 3rd call back to progress bar on adjustments: Westfall Young Single Step and Step Down is done
   if (is.function(updateProgress) & !is.null(adjp.all)) {
     msg  <- paste0("Multiple adjustments done for WestFall Young SS & SD.") # Priamry text we want to display
     updateProgress(message = msg) # Passing back the progress messages onto the callback function
   } # if the function is being called, run the progress bar
 
-  # for each MTP, get matrix of indicators of whether adjusted p-value is less than alpha
+  # for each MTP, get matrix of indicators for whether the adjusted p-value is less than alpha
   reject<-function(x) {as.matrix(1*(x<alpha))}
   reject.all<-lapply(adjp.all,reject)
 
-  # in each row for each MTP matrix, count number of p-values less than 0.05, in rows corresponding to false nulls
+  # Helper function: In each row for each MTP matrix, count number of p-values less than 0.05,
+  # in rows corresponding to false nulls
   lt.alpha<-function(x) {apply(as.matrix(x[,MDES>0]),1,sum)}
-
   lt.alpha.all<-lapply(reject.all,lt.alpha)
-  # indiv power for WY, BH, and HO is mean of columns of dummies of whether adjusted pvalues were less than alpha
+
+  # indiv power for WY-SS, WY-SD, BH, HO, BF is mean of columns of booleans of whether adjusted pvalues were less than alpha
+  # in other words, the null has been rejected
   power.ind.fun<-function(x) {apply(x,2,mean)}
   power.ind.all<-lapply(reject.all,power.ind.fun)
   power.ind.all.mat<-do.call(rbind,power.ind.all)
 
-  #4th call back to progress bar: Individual Power calculation done
+  # 4th call back to progress bar: Individual power calculations are done
   if (is.function(updateProgress) & !is.null(power.ind.all.mat)) {
     msg  <- paste0("Individual power calculation is done.") # Priamry text we want to display
     updateProgress(message = msg) # Passing back the progress messages onto the callback function
   } # if the function is being called, run the progress bar
 
-  # m-min powers for all procs (including complete power when m=M)
+  # Helper function: m-min powers for all MTPs (including complete power when m=M)
   power.min.fun <- function(x,M) {
     power.min<-numeric(M)
     cnt<-0
@@ -265,28 +294,28 @@ power.blockedRCT.2<-function(M, MDES, Ai, J, n.j,
       cnt<-cnt+1
     }
     return(power.min)
-  }
+  } # end of calculating d-minimal power
 
+  # calculating d-minimal power
   power.min<-lapply(lt.alpha.all,power.min.fun,M=M)
-  #browser()
   power.min.mat<-do.call(rbind,power.min)
-  #browser()
-  # complete power is probability all false nulls rejected when p-values not adjusted
-  # this is row 1 and column number = numfalse
+
+  # complete power is the power to detect outcomes at least as large as the MDES on all outcomes
+  # separating out complete power from d-minimal power by taking the last entry
   power.cmp<-rep(power.min.mat[1,M],length(power.min)) # should it be numfalse or M?
-  #browser()
+
   # combine all power for all definitions
   all.power.results<-cbind(power.ind.all.mat,power.min.mat[,-M],power.cmp)
-  #browser()
-  # take mean of all individual power estimates
+
+  # calculating average individual power
   mean.ind.power <- apply(as.matrix(all.power.results[,1:M][,MDES>0]),1,mean)
-  #browser()
+
   # revise final matrix to report this mean individual power and return results
   all.power.results<-cbind(mean.ind.power,all.power.results)
-  #browser()
-  colnames(all.power.results)<-c("indiv",paste0("indiv",1:M),paste0("min",1:(M-1)),"complete")
+
+  # setting the col and row names for all power results table
+  colnames(all.power.results)<-c("average indiv",paste0("indiv",1:M),paste0("min",1:(M-1)),"complete")
   rownames(all.power.results)<-c("rawp","BF","HO","BH","WY-SS","WY-SD")
-  #browser()
 
   #5th call back to progress bar: All power calculation done
   if (is.function(updateProgress) & !is.null(all.power.results)) {
